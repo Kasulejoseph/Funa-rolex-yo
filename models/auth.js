cda652c463376333b5deb0eedb4409081702b118
kasule joseph <joseph.kasule@andela.com>
2019-08-15 17:09:27 +0200

    ch(postgres): setup postgres
    - make a connection to local postgres
    
    [Delivers #167895894]

---
+++ models/auth.js
@@ -0,0 +1,46 @@
+import pool from './connection'
+
+const users = () => {
+    const createUser = 
+    `
+    CREATE TABLE IF NOT EXISTS users(
+        id UUID PRIMARY KEY,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        phone_number TEXT NOT NULL,
+        address TEXT NOT NULL,
+        email TEXT,
+        password TEXT NOT NULL
+    )
+    `;
+
+    pool.query(createUser).then(
+        (res) => {
+            console.log(res);
+            pool.end()   
+        }
+    ).catch((err) => {
+        console.log(err);
+        pool.end()
+    })
+    
+}
+
+const dropTables = () => {
+    const queryText = 'DROP TABLE IF EXISTS users';
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end()  
+        }).catch((err) => {
+            console.log(err);
+            pool.end()
+        })
+}
+
+module.exports = {
+    users,
+    dropTables
+}
+
+require('make-runnable')
